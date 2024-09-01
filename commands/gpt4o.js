11744199ed1d74696d80a16c9a9819e051aefe6f
clairechatbot69 <clairechatbot@gmail.com>
2024-09-01 17:56:33 +0800

    Update gpt4o.js

--- commands/gpt4o.js
+++ commands/gpt4o.js
@@ -28,7 +28,7 @@ module.exports = {
         if (!prompt) return api.sendMessage("Please enter a prompt.", threadID, messageID);
 
         api.sendMessage(
-            "✴✶ 🎀 GCHAT BOT 🎀 ✶✴\n\n" +
+            "✴✶ 🎀 𝗚𝗘𝗢 𝗧𝗘𝗖𝗛 𝗔𝗜 🎀 ✶✴\n\n" +
             "⏳ Responding...",
             threadID,
             async (err, info) => {
@@ -36,7 +36,7 @@ module.exports = {
                 try {
                     const response = await fetchAIResponse(prompt);
                     api.editMessage(
-                        "✴✶ 🎀 GCHAT BOT 🎀 ✶✴\n\n" +
+                        "✴✶ 🎀 𝗚𝗘𝗢 𝗧𝗘𝗖𝗛 𝗔𝗜 🎀 ✶✴\n\n" +
                         response,
                         info.messageID
                     );
