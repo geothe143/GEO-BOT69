d5ed287ef36ef16f47841ab06764f8801e4f49ae
clairechatbot69 <clairechatbot@gmail.com>
2024-09-05 05:39:00 +0800

    Update mixtral.js

--- commands/mixtral.js
+++ commands/mixtral.js
@@ -6,7 +6,7 @@ async function fetchMixtralResponse(prompt) {
         if (response.data.status) {
             return response.data.result;
         } else {
-            return "Failed to get a valid response from the Mixtral AI.";
+            return "Failed to get a valid response from the g3 AI.";
         }
     } catch (error) {
         console.error("Error fetching data:", error.message);
@@ -15,28 +15,28 @@ async function fetchMixtralResponse(prompt) {
 }
 
 module.exports = {
-    name: "mixtral",
-    description: "Talk to Mixtral AI",
+    name: "g3",
+    description: "Talk to G3 AI",
     nashPrefix: false,
     version: "1.0.0",
     role: 0,
     cooldowns: 5,
-    aliases: ["mixtral", "mistral"],
+    aliases: ["g3", "g3"],
     execute(api, event, args, prefix) {
         const { threadID, messageID } = event;
         let prompt = args.join(" ");
         if (!prompt) return api.sendMessage("Please enter a prompt for the Mixtral AI.", threadID, messageID);
 
         api.sendMessage(
-            "[ 𝙈𝙄𝙓𝙏𝙍𝘼𝙇 ]\n\n" +
-            "⏳ Fetching the response...",
+            "🎀 𝗚𝗘𝗢 𝗧𝗘𝗖𝗛 𝗔𝗜69 🎀\n\n" +
+            "💬 Responding...",
             threadID,
             async (err, info) => {
                 if (err) return;
                 try {
                     const response = await fetchMixtralResponse(prompt);
                     api.editMessage(
-                        "[ 𝙈𝙄𝙓𝙏𝙍𝘼𝙇 ]\n\n" +
+                        "🎀 𝗚𝗘𝗢 𝗧𝗘𝗖𝗛 𝗔𝗜69 🎀\n\n" +
                         response,
                         info.messageID
                     );
