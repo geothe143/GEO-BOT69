f8dd2d8f614b3251f8e54c29238ce37e22ae1154
clairechatbot69 <clairechatbot@gmail.com>
2024-09-05 18:51:08 +0800

    Update hentagif.js

--- commands/hentagif.js
+++ commands/hentagif.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = {
-  name: 'hentagif',
+  name: 'bawalpindutin',
   description: 'hentai gif.',
   nashPrefix: false,
   execute(api, event, args, prefix) {
