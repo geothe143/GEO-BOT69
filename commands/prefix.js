533613e72c4426869a0a99fa7614b3f568df3996
clairechatbot69 <clairechatbot@gmail.com>
2024-09-08 21:54:24 +0800

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -6,7 +6,7 @@ module.exports = {
   nashPrefix: false,
   execute(api, event, args, prefix) {
     try {
-      const message = `Yo, my prefix is [ 𓆩 '${prefix}' 𓆪 ]\n\n𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:\n➥ '${prefix}help [command] -> information and usage of command\n\nHave fun using it, enjoy! ❤`;
+      const message = `🎀 𝗚𝗘𝗢 𝗧𝗘𝗖𝗛 𝗔𝗜69 🎀 [ 𓆩 '${prefix}' 𓆪 ]\n\n𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:\n➥ '${prefix}help [command] -> information and usage of command\n\nHave fun using it, enjoy! ❤`;
       
       const imagePath = './josh.jpeg';
 
