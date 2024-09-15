f12f7c293251dbed6ebdb10768c8ec17345bf1d0
geothe143 <geothe135@gmail.com>
2024-09-15 20:55:10 +0800

    Update ai.js

--- commands/ai.js
+++ commands/ai.js
@@ -26,7 +26,7 @@ module.exports = {
         const { threadID, messageID, senderID } = event;
         const message = args.join(" ");
 
-        if (!message) return api.sendMessage("Please provide your question.\n\nExample: G4 What is the solar system?", threadID, messageID);
+        if (!message) return api.sendMessage("Please provide your question.\n\nExample: ai pogi ba si George Nakila?", threadID, messageID);
 
         api.sendMessage(
             "💬 Responding...",
